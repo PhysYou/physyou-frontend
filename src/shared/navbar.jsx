f7178835e5e0e162a8e3725995579178f2e4b7d4
Yi Zu <zuyi@bu.edu>
2021-02-13 13:51:31 +0100

    Gradient to navbar and code formatting

--- src/shared/navbar.jsx
+++ src/shared/navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,13 +6,16 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import {UserContext} from "../providers/UserProvider";
-import {auth} from "../firebase";
-import {useHistory} from 'react-router-dom';
+import { UserContext } from "../providers/UserProvider";
+import { auth } from "../firebase";
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+    },
+    navbar: {
+        background: 'linear-gradient(to right, #3553ae, #6394dd)',
     },
     menuButton: {
         marginRight: theme.spacing(2),
@@ -28,7 +31,7 @@ export default function ButtonAppBar() {
     const history = useHistory();
 
     const handleLog = () => {
-        if(user) {
+        if (user) {
             auth.signOut();
             window.location.reload();
         } else {
@@ -38,7 +41,7 @@ export default function ButtonAppBar() {
 
     return (
         <div className={classes.root}>
-            <AppBar position="static">
+            <AppBar className={classes.navbar} position="static">
                 <Toolbar>
                     {/*<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">*/}
                     {/*    <MenuIcon />*/}
