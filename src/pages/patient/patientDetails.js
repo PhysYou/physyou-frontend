3287aa8e2df24f258acd445e8017e69ce3931a56
Yi Zu <zuyi@bu.edu>
2021-02-13 18:29:53 +0100

    Added patientDetails page

--- src/pages/patient/patientDetails.js
+++ src/pages/patient/patientDetails.js
@@ -1,15 +1,57 @@
 import React from 'react';
-import { Typography, Paper, Box } from '@material-ui/core';
+import { Avatar, Button, Typography, Paper, Box, makeStyles } from '@material-ui/core';
 import ClearIcon from '@material-ui/icons/Clear';
 import ExerciseDetails from '../../shared/exerciseDetails';
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        // marginTop: theme.spacing(8),
+        width: '22vw',
+        height: '95vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    avatar: {
+        width: theme.spacing(10),
+        height: theme.spacing(10),
+        margin: 'auto',
+        backgroundColor: theme.palette.secondary.main,
+    },
+}));
+
+
 export default function PatientDetails() {
+    const classes = useStyles();
+
+    const handleClose = () => {
+        console.log('Close')
+    }
+
     return (
-        <Paper style={{ width: '70vw', height: '45vh' }}>
+        <>
+        <Paper className={classes.paper}>
+                <Box textAlign='center'>
+                    <Box mb={2}>
+                        <Avatar className={classes.avatar}></Avatar>
+                    </Box>
+                    <Typography variant='h6'>Dr. Perez, M.D</Typography>
+                    <Typography variant='h7' color='secondary'>Since February 2021</Typography>
+                    <Typography variant='body2'>Notes on diagnosis, plan</Typography>
+                    <Box mt={8}>
+                        <Button>Message</Button>
+                    </Box>
+                </Box>
+
+             
+        </Paper>
+
+        {/* <Paper style={{ width: '70vw', height: '45vh' }}>
             <Box p={6}>
             <Box display='flex' justifyContent='space-between'>
-                    <Typography variant='h3'>Exercise</Typography>
-                    <ClearIcon fontSize='large' style={{ fill: '#6ed3c4'}} />
+                    <Typography variant='h3'>Exercise Name</Typography>
+                    <ClearIcon onClick={handleClose} fontSize='large' style={{ fill: '#6ed3c4', cursor: 'pointer'}} />
             </Box>
             <Box display='flex' justifyContent='center'>
                 <ExerciseDetails />
@@ -18,6 +60,7 @@ export default function PatientDetails() {
                 <Typography variant='h6'>Exercise notes here</Typography>
             </Box>
             </Box>
-        </Paper>
+            </Paper> */}
+        </>
     );
 }
