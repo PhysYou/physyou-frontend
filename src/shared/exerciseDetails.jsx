5c160d77dd20a4830c9668dd927ab2d021fdfb13
Yi Zu <zuyi@bu.edu>
2021-02-13 14:40:56 +0100

    Finalized style and center-text

--- src/shared/exerciseDetails.jsx
+++ src/shared/exerciseDetails.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Box, List, ListItem, ListItemText, fade } from '@material-ui/core';
+import { Box, List, ListItem, ListItemText } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
 
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     listItem: {
-        width: '70vw',
+        width: '60vw',
         height: '80px',
         background: '#f8f8f8',
         borderRadius: '5px',
         marginTop: '12px',
+        display: 'flex',
+        textAlign: 'center',
     },
-
 }));
 
 const darkText = {
@@ -31,31 +32,18 @@ function ExerciseDetails() {
     const classes = useStyles();
 
     return (
-        <>
-            <List>
-                <ListItem className={classes.listItem} key='1'>
-                    <Box mr={2}>
-                        <FitnessCenterIcon style={{ fill: "#2DD6C2" }} />
-                    </Box>
-                    <ListItemText ml={4} primaryTypographyProps={{ style: lightText }}>COMPLETED</ListItemText>
-                    <Box display='flex'>
-                        <Box pl={5} mr={5}>
-                            <ListItemText primaryTypographyProps={{ style: lightText }}>90% ACCURACY</ListItemText>
-                        </Box>
-                        <Box mr={5}>
-                            <ListItemText primaryTypographyProps={{ style: darkText }}>24 reps</ListItemText>
-                        </Box>
-                        <Box mr={5}>
-                            <ListItemText primaryTypographyProps={{ style: darkText }}>10 minutes</ListItemText>
-                        </Box>
-                        <Box>
-                            <ListItemText primaryTypographyProps={{ style: darkText }}>Intesity: High</ListItemText>
-                        </Box>
-                    </Box>
-                </ListItem>
-            </List>
-        </>
-
+        <List >
+            <ListItem className={classes.listItem} key='1'>
+                <Box display='flex'>
+                    <FitnessCenterIcon style={{ fill: "#2DD6C2", marginRight: '16px' }} />
+                    <ListItemText primaryTypographyProps={{ style: lightText }}>COMPLETED</ListItemText>
+                </Box>
+                <ListItemText primaryTypographyProps={{ style: lightText }}>90% ACCURACY</ListItemText>
+                <ListItemText primaryTypographyProps={{ style: darkText }}>24 reps</ListItemText>
+                <ListItemText primaryTypographyProps={{ style: darkText }}>10 minutes</ListItemText>
+                <ListItemText primaryTypographyProps={{ style: darkText }}>Intesity: High</ListItemText>
+            </ListItem>
+        </List>
     );
 }
 
