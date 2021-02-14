cc4e31110d0346b8338bf244b2dd61cd1122c3aa
Ian Perez <iperez@mit.edu>
2021-02-13 21:30:15 -0400

    Setting doctor interaction

--- src/pages/patient/index.js
+++ src/pages/patient/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Box, Paper} from '@material-ui/core';
 import Navbar from '../../shared/navbar';
 import ExerciseList from '../../shared/exerciseList';
@@ -7,18 +7,22 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import SideBar from "../../shared/sideBar";
 import DatePicker from "react-horizontal-datepicker";
+import {UserContext} from "../../providers/UserProvider";
 
 export default function PatientHome() {
-    const doctor = {
-        name: 'Ian Perez',
-        start: 'since May 2019',
-        blurb: 'Blurb about doctor and what he specializes in etc. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor '
+    // const doctor = {
+    //     name: 'Ian Perez',
+    //     start: 'since May 2019',
+    //     blurb: 'Blurb about doctor and what he specializes in etc. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor '
+    //
+    // }
 
-    }
+    const user = useContext(UserContext)
+
     return (
         <>
             <Box display='flex'>
-                <SideBar/>
+                <SideBar person={user?.doctor ?? null}/>
                 <Box display='flex' flexDirection='column' flexGrow={1}>
                     <Box width='calc(100vw - 300px)' style={{backgroundColor: '#D5DFEF'}} py={3} px={2}>
                         <DatePicker getSelectedDay={(date) => console.log(date)}/>
--- src/shared/sideBar.jsx
+++ src/shared/sideBar.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import {
     Avatar,
     Button,
     Typography,
     Paper,
     Box,
-    makeStyles,
+    makeStyles, TextField,
 } from '@material-ui/core';
+import {firestore} from "../firebase";
+import {UserContext} from "../providers/UserProvider";
 
 const useStyles = makeStyles((theme) => ({
     sideBar: {
@@ -33,24 +35,51 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-function SideBar() {
+function SideBar({person}) {
     const classes = useStyles();
+    const user = useContext(UserContext);
+    const [email, setEmail] = useState('');
+
+    function handleSubmit() {
+        let usersRef = firestore.collection('users')
+        let query = usersRef.where('email', "==", email).where('type', '==', 'doctor')
+        query.get().then(users => {
+            if(!users.empty){
+                let doctor = users.docs[0].data();
+                let userDoc = usersRef.doc(user.uid);
+                return userDoc.update({doctor: doctor.id})
+            } else {
+                alert("Error fetching")
+            }
+
+        })
+    }
+
+    const item = person
+        ? <Box textAlign='center'>
+            <Box mb={2}>
+                <Avatar className={classes.avatar}></Avatar>
+            </Box>
+            <Typography variant='h6'>Dr. Perez, M.D</Typography>
+            <Typography variant='h7' color='secondary'>
+                Since February 2021
+            </Typography>
+            <Typography variant='body2'>Notes on diagnosis, plan</Typography>
+            <Box mt={8}>
+                <Button className={classes.msgBtn}>Message</Button>
+            </Box>
+        </Box>
+        : <Box display="flex" flexDirection="column">
+            <TextField label="Doctor's Email" variant='filled' placeholder="Type you doctor's email"/>
+            <Button variant='outlined' color='secondary' style={{marginTop: '10px'}} onClick={handleSubmit}>Submit</Button>
+        </Box>
+
 
     return (
         <Paper className={classes.sideBar}>
-            <Box textAlign='center'>
-                <Box mb={2}>
-                    <Avatar className={classes.avatar}></Avatar>
-                </Box>
-                <Typography variant='h6'>Dr. Perez, M.D</Typography>
-                <Typography variant='h7' color='secondary'>
-                    Since February 2021
-            </Typography>
-                <Typography variant='body2'>Notes on diagnosis, plan</Typography>
-                <Box mt={8}>
-                    <Button className={classes.msgBtn}>Message</Button>
-                </Box>
-            </Box>
+            {
+                item
+            }
         </Paper>
     )
 }
