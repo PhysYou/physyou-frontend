61fab42bd439d9d4c8e9eb04b043a00d27e37b45
Yi Zu <zuyi@bu.edu>
2021-02-14 17:10:12 +0100

    Added logo to navbar

--- src/shared/navbar.jsx
+++ src/shared/navbar.jsx
@@ -2,13 +2,14 @@ import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { UserContext } from "../providers/UserProvider";
 import { auth } from "../firebase";
 import { useHistory } from 'react-router-dom';
+import { ReactComponent as Logo } from './logo.svg';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
     menuButton: {
         marginRight: theme.spacing(2),
     },
-    title: {
+    logoWrapper: {
         flexGrow: 1,
     },
 }));
@@ -30,25 +31,21 @@ export default function ButtonAppBar() {
     const user = useContext(UserContext);
     const history = useHistory();
 
-    const handleLog = () => {
+    const handleLog = async () => {
         if (user) {
-            auth.signOut();
-            window.location.reload();
-        } else {
-            history.push('/login');
+            await auth.signOut();
         }
+        console.log("HERE")
+        history.push('/login');
     }
 
     return (
         <div className={classes.root}>
             <AppBar className={classes.navbar} position="static">
                 <Toolbar>
-                    {/*<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">*/}
-                    {/*    <MenuIcon />*/}
-                    {/*</IconButton>*/}
-                    <Typography variant="h6" className={classes.title}>
-                        PhysYou
-                    </Typography>
+                    <Box className={classes.logoWrapper}>
+                        <Logo style={{ height: 30, width: 150, }} />
+                    </Box>
                     <Button color="inherit" size="large">Exercises</Button>
                     <Button color="inherit" size="large">Messages</Button>
                     <Button color="inherit" size="large" onClick={handleLog}>{user ? 'Logout' : 'Login'}</Button>
